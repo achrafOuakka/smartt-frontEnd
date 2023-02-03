afced102e21012c256a331ef9335ea23390417ab
unknown <a.ouakka@distinktec.com>
2023-02-03 17:29:15 +0100

    add section portfolio

--- src/app/components/main/portfolio/portfolio.component.ts
+++ src/app/components/main/portfolio/portfolio.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  listPortfolio : Array<{img: string, title : string, category:string}> = [];
+  constructor() {
+    this.listPortfolio = [
+      {img: 'portfolio-1.jpg', title : 'App 1', category:'app'},
+      {img: 'portfolio-2.jpg', title : 'Web 3', category:'web'},
+      {img: 'portfolio-3.jpg', title : 'App 2', category:'app'},
+      {img: 'portfolio-4.jpg', title : 'card 2', category:'card'},
+      {img: 'portfolio-5.jpg', title : 'Web 2', category:'web'},
+      {img: 'portfolio-6.jpg', title : 'App 3', category:'app'},
+      {img: 'portfolio-7.jpg', title : 'Card 1', category:'card'},
+      {img: 'portfolio-8.jpg', title : 'Card 3', category:'card'},
+      {img: 'portfolio-9.jpg', title : 'Web 1', category:'web'},
+    ];
+  }
 
   ngOnInit(): void {
   }
