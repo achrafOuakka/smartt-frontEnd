c3cbb99141066198e5ad35cf68c8d0c143068ef0
unknown <a.ouakka@distinktec.com>
2023-02-07 13:05:57 +0100

    rename an Angular project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/siteWeb-frontEnd'),
+      dir: require('path').join(__dirname, './coverage/smartt-frontEnd'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'siteWeb-frontEnd';
+  title = 'smartt-frontEnd';
 }
