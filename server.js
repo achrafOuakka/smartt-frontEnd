42c66fec90efd7a59af1637ede2e48d4eb5e25da
unknown <a.ouakka@distinktec.com>
2023-02-22 16:30:02 +0100

    more update in file server.js to Node.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolio-angular11-dataLocal'));
+app.use(express.static('./dist/smartt-frontEnd'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/portfolio-angular11-dataLocal/' }),
+    res.sendFile('index.html', { root: 'dist/smartt-frontEnd/' }),
 );
 
 // Start the app by listening on the default Heroku port
